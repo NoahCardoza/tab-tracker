b2762b1202a108943cc1c9b0ef94af6af3050a0b
Noah Cardoza <noahcardoza@gmail.com>
2017-12-27 13:01:46 -0800

    Set up express and removed node_modules from git

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
-console.log(42)
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 42
+  })
+})
+
+app.listen(process.env.PORT || 8081)
