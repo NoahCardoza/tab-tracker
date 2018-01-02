58d7925e6eaa8890098eb609fcdb870852bf49d5
Noah Cardoza <noahcardoza@gmail.com>
2018-01-02 10:07:05 -0800

    Now uses arrow functions

--- server/src/policies/IsAuthenticated.js
+++ server/src/policies/IsAuthenticated.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 
 module.exports = {
   hard (req, res, next) {
-    passport.authenticate('jwt', function (err, user) {
+    passport.authenticate('jwt', (err, user) => {
       if (err || !user) {
         res.status(403).send({
           error: "You do not have access to this resource."
@@ -14,7 +14,7 @@ module.exports = {
     })(req, res, next)
   },
   soft (req, res, next) {
-    passport.authenticate('jwt', function (err, user) {
+    passport.authenticate('jwt', (err, user) => {
         req.user = user
         next()
     })(req, res, next)
